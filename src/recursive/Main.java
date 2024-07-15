3042aae48efef4084bf39136f6e5283d5ac8e6ba
kihyuk.jeong <kihyuk.jeong@musinsa.com>
2024-07-15 21:27:15 +0900

    recursive

---
+++ src/recursive/Main.java
@@ -0,0 +1,27 @@
+package recursive;
+
+public class Main {
+
+    public void DFS(int n) {
+
+        if (n == 0) {
+            return;
+        } else {
+//            DFS(n - 1);
+            System.out.println(n);
+            DFS(n - 1);
+
+
+        }
+
+    }
+
+    public static void main(String[] args) {
+
+        Main main = new Main();
+        main.DFS(3);
+
+    }
+
+
+}
